0fa8d9a151d6638c36b47cbcaf1a57c0f6ef1915
ohlafolle <pinault.alisson@gmail.com>
2025-09-15 16:37:05 +0200

    Configuration JawsDB pour Heroku

--- php/db.php
+++ php/db.php
@@ -1,24 +1,27 @@
 <?php
 // --------------------
-// Connexion MySQL via PDO
+// Connexion MySQL via JawsDB
 // --------------------
+$cleardb_url = parse_url(getenv("JAWSDB_URL")); // récupère l'URL de l'addon
+$servername = $cleardb_url["host"];
+$username = $cleardb_url["user"];
+$password = $cleardb_url["pass"];
+$dbname = substr($cleardb_url["path"], 1);
+
 try {
-    $pdo = new PDO(
-        'mysql:host=mysql;dbname=bookshare;charset=utf8',
-        'user',
-        'password'
-    );
+    $pdo = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erreur MySQL : " . $e->getMessage());
 }
 
 // --------------------
-// Connexion MongoDB
+// Connexion MongoDB (Atlas ou mLab)
 // --------------------
 require 'vendor/autoload.php';
 use MongoDB\Client;
 
-$mongoClient = new Client("mongodb://mongo:27017");
+$mongoUri = getenv('MONGODB_URI') ?: 'mongodb://localhost:27017';
+$mongoClient = new Client($mongoUri);
 $mongoDB = $mongoClient->bookshare;
 ?>
