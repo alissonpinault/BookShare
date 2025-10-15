c7704395a13abc67574e30e0a7eb6eb6a80ee7e2
ohlafolle <pinault.alisson@gmail.com>
2025-10-15 12:27:50 +0200

    correct

--- public/admin.php
+++ public/admin.php
@@ -377,12 +377,6 @@ if (isset($_GET['message']) && $_GET['message'] !== '') {
 }
 ?>
 
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-?>
-
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
